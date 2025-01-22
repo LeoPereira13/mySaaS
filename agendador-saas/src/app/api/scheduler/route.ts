5b27bd7e0ccf45b406e0df6ace8de6af9c223e9f
Leo Pereira <leo.dpsouza@gmail.com>
2025-01-21 21:42:54 -0300

    configurações do prisma

--- agendador-saas/src/app/api/scheduler/route.ts
+++ agendador-saas/src/app/api/scheduler/route.ts
@@ -1,27 +1,26 @@
+import prisma from "@/utils/prisma";
+
 export async function GET() {
-    try {
-      console.log("Requisição recebida: GET /schedules");
-  
-      // Simulação de retorno de agendamentos
-      const schedules = [
-        { id: 1, title: "Reunião com João", time: "2025-01-08T10:00:00Z" },
-        { id: 2, title: "Revisão do projeto", time: "2025-01-09T15:00:00Z" },
-      ];
-  
-      console.log("Agendamentos retornados:", schedules);
-  
-      return new Response(JSON.stringify(schedules), {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      });
-    } catch (error) {
-      console.error("Erro no servidor:", error);
-      return new Response(JSON.stringify({ error: "Erro no servidor." }), {
-        status: 500,
-        headers: { "Content-Type": "application/json" },
-      });
-    }
+  try {
+    const schedules = await prisma.schedule.findMany({
+      include: {
+        user: true,  // Inclui os dados do usuário relacionado
+      },
+    });
+
+    return new Response(JSON.stringify(schedules), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Erro ao buscar agendamentos:", error);
+    return new Response(JSON.stringify({ error: "Erro no servidor" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
+}
+
   
   export async function POST(req: Request) {
     try {
